b90915b8b613c10cb6cf2665eb2a3c209a0ae57a
loderq <2650009628@qq.com>
2020-11-03 23:01:23 +0800

    link: Update.

--- link.php
+++ link.php
@@ -1,7 +1,7 @@
 <?php
 	$host="127.0.0.1";
-	$root="ljh";
-	$pwd="123456";
+	$root="root";
+	$pwd="password";
 	$link=@mysqli_connect($host,$root,$pwd) or die("数据库连接失败");
 	mysqli_select_db($link,'hldx');
 	mysqli_set_charset($link,'utf8');
